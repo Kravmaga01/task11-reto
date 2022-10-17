87851d926e8b888489401a5a3e01452e0f121925
Kravmaga 01 <92754288+Kravmaga01@users.noreply.github.com>
2022-10-16 23:22:14 -0500

    [add:] component authors and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthorComponent } from './authors/author/author.component';
+import { ListComponent } from './authors/list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AuthorComponent, ListComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
